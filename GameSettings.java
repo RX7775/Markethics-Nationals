5d16cd8d7509f5b45ef200964d444c3474867b54
Richard Xu <96448524+RX7775@users.noreply.github.com>
2025-03-27 16:14:52 -0500

    Update GameSettings.java

--- GameSettings.java
+++ GameSettings.java
@@ -1,5 +1,6 @@
 public class GameSettings {
     private static int numDays = 30; // Default game duration
+    private static int difficulty = 1; // 1 for easy, 2 for medium, 3 for hard
 
     public static int getNumDays() {
         return numDays;
@@ -7,5 +8,13 @@ public class GameSettings {
 
     public static void setNumDays(int days) {
         numDays = days;
+    }
+    
+    public static int getDifficulty() {
+        return difficulty;
+    }
+    
+    public static void setDifficulty(int dif) {
+        difficulty = dif;
     }
 }
